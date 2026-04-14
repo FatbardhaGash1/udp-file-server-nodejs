ac54a64dda6f9578f0ab5edfbc5783fdac5d381b
Bardha <fatbardha.gashi7@student.uni-pr.edu>
2026-04-14 21:27:01 +0200

    nis UDP dhe HTTP servers

--- server/server.js
+++ server/server.js
@@ -2,4 +2,11 @@ const dgram = require('dgram');
 const udpHandler = require('./udpHandler');
 const httpServer = require('./httpServer');
 const PORT = 41234;
-const HOST = '127.0.0.1';
+const HOST = '0.0.0.0';
+
+const server = dgram.createSocket('udp4');
+udpHandler.init(server);
+server.bind(PORT, HOST, () => {
+  console.log(`UDP Server running at ${HOST}:${PORT}`);
+});
+httpServer.start(5000);
