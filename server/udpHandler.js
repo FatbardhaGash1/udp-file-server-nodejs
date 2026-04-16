02d5788ee2b602318cec3dd8bdec277f75a00183
Bardha <fatbardha.gashi7@student.uni-pr.edu>
2026-04-16 18:09:11 +0200

    regjistrimi i klientëve dhe përpunimin i komandave

--- server/udpHandler.js
+++ server/udpHandler.js
@@ -2,3 +2,24 @@ const fileManager = require('./fileManager');
 const clientManager = require('./clientManager');
 
 let server;
+function init(srv) {
+  server = srv;
+
+  server.on('message', (msg, rinfo) => {
+    const message = msg.toString().trim();
+    const clientId = `${rinfo.address}:${rinfo.port}`;
+
+    clientManager.registerClient(clientId, rinfo);
+    clientManager.updateActivity(clientId);
+
+    let response;
+
+    try {
+      response = handleCommand(message, clientId);
+    } catch (err) {
+      response = 'Error: ' + err.message;
+    }
+
+    server.send(Buffer.from(response), rinfo.port, rinfo.address);
+  });
+}
