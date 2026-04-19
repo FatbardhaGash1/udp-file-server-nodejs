3a3a5570420d18a6ecfecccf5862d5e817173cb9
Festa10 <festa.berisha3@student.uni-pr.edu>
2026-04-19 20:41:08 +0200

    Updated httpServer

--- server/httpServer.js
+++ server/httpServer.js
@@ -1,11 +1,22 @@
 const http = require('http');
 const clientManager = require('./clientManager');
+const udpHandler = require('./udpHandler');
 
 function start(port) {
     const server = http.createServer((req, res) => {
         if (req.url === '/stats') {
+            const messages = udpHandler.getMessages();
+
+
+            const stats = {
+                activeClients: clientManager.getStats().activeClients,
+                clients: clientManager.getStats().clients,
+                messageCount: messages.length,
+                messages: messages
+            };
+                
             res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(clientManager.getStats(), null, 2));
+            res.end(JSON.stringify(stats, null, 2));
         } else {
             res.writeHead(404);
             res.end('Not found');
